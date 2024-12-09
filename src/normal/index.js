941da193f6b5bdaa2c8791e59d345f6adb97d24a
Naka <bacondoor1313@gmail.com>
2024-12-09 13:30:06 +0900

    good

--- src/normal/index.js
+++ src/normal/index.js
@@ -66,7 +66,7 @@ app.assets.loadFromUrl(modelUrl, 'gsplat', function (err, asset) {
 
     asset.ready(function (resource) {
         console.log(entity.gsplat.instance.material)
-    entity.gsplat.instance.material.blendType = pc.BLEND_PREMULTIPLIED;
+    entity.gsplat.instance.material.blendType =pc.BLEND_NORMAL;
     entity.gsplat.instance.material.update();
     console.log(entity.gsplat.instance.material)
     }
